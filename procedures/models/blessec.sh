d63c5b2a1fdecd26792c9c73634e68449302e98e
Greenstick <benacordier@gmail.com>
2017-01-31 14:39:39 -0800

    Version: 0.4.1 - Blessec Bug Fix

--- procedures/models/blessec.sh
+++ procedures/models/blessec.sh
@@ -78,8 +78,8 @@ if !(has_state $state); then
 
      format_status "Splitting Paired End FASTQ to Single End"
      # Call Error Model
-     format_status "Command:\nfastqutils unmerge $fileprefix.$subset.$condition.$experiment.$parameters.$readgroup.fastq $fileprefix.$subset.$condition.$experiment.$parameters.$readgroup.split"
-     perl $PAIRFQ splitpairs -i $dataDir/fastq/split/$fileprefix.$subset.$condition.$experiment.$parameters.$readgroup.fastq -f $dataDir/fastq/split/$fileprefix.$subset.$condition.$experiment.$parameters.$readgroup.split.1.fastq -r $dataDir/fastq/split/$fileprefix.$subset.$condition.$experiment.$parameters.$readgroup.split.2.fastq
+     format_status "Command:\nfastqutils unmerge $fileprefix.$subset.$condition.$readgroup.fastq $fileprefix.$subset.$condition.$readgroup.split"
+     perl $PAIRFQ splitpairs -i $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.fastq -f $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.split.1.fastq -r $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.split.2.fastq
      # Update State on Exit
      status=$?
      put_state $status $state
