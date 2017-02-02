c33e75bcda91bbed25cc31b5c286781301626b3c
Greenstick <benacordier@gmail.com>
2017-02-02 13:08:06 -0800

    Version: 0.5.0 - Updated Fast/Q Conversion Utils

--- utils/fasta-qual-to-fastq.py
+++ utils/fasta-qual-to-fastq.py
@@ -4,50 +4,40 @@ if __name__ == "__main__":
 
 
 	# Imports
-	import sys, getopt
+	import sys, argparse
 	# Library Import
 	from Bio import SeqIO
 	from Bio.SeqIO.QualityIO import PairedFastaQualIterator
-
-	# Define References
-	fasta = ""
-	qual = ""
-	prefix = ""
 
 	# 
 	# Parse Arguments
 	# 
 
-	try:
-		opts, args = getopt.getopt(sys.argv, "hf:q:o", ["--inputfasta", "--inputqual", "--outputprefix", "--help"])
-	except getopt.GetoptError:
-		print("fasta-qual to fastq.py -f <input fasta> -q <input qual> -o <output prefix>")
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt in ("-h", "--help"):
-			print("Merge Fasta and Qual files into a FastQ file.\nExample usage:\nfasta-qual-to-fastq.py -f <input fasta> -q <input qual> -o <output prefix>")
-		elif opt in ("-f", "--inputfasta"):
-			fasta = arg
-		elif opt in ("-q", "--inputqual"):
-			qual = arg
-		elif opt in ("-o", "--outputprefix"):
-			prefix = arg
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-f", "--inputfasta", type = str, help = "Input Fasta File")
+	parser.add_argument("-q", "--inputqual", type = str, help = "Input Qual File")
+	parser.add_argument("-o", "--outputprefix", type = str, help = "Prefix to Output FastQ File")
+	argsDict = vars(parser.parse_args())
+
+	fasta = argsDict["inputfasta"]
+	qual = argsDict["inputqual"]
+	prefix = argsDict["outputprefix"]
 
 	# Assertions for Required Input
-	assert (len(fasta) != 0), "No Fasta input provided!"
-	assert (len(qual) != 0), "No Qual input provided!"
+	assert (fasta is not None), "No Fasta input provided!"
+	assert (qual is not None), "No Qual input provided!"
 
 	# 
 	# Conversion
 	#
 
 	#  If No Prefix, Use Same as FASTQ
-	if len(prefix) < 1: 
-		prefix = fasta.split(".")[0:-1]
+	if prefix is None: 
+		prefix = ".".join(fasta.split(".")[0:-1])
 
 	# Merge Fasta & Qual into FastQ
-	records = PairedFastaQualIterator(open(fasta), open(qual))
-	SeqIO.write(records, prefix + ".fastq", "fastq")
+	with PairedFastaQualIterator(open(fasta), open(qual)) as records:
+		SeqIO.write(records, prefix + ".fastq", "fastq")
 
 
 else:
--- utils/fastq-to-fasta-qual.py
+++ utils/fastq-to-fasta-qual.py
@@ -4,43 +4,32 @@ if __name__ == "__main__":
 
 
 	# Imports
-	import sys, getopt
+	import sys, argparse
 	# Library Import
 	from Bio import SeqIO
-
-	# Define References
-	fastq = ""
-	prefix = ""
 
 	# 
 	# Parse Arguments
 	# 
 
-	try:
-		opts, args = getopt.getopt(sys.argv, "hi:o", ["--input", "--outputprefix", "--help"])
-	except getopt.GetoptError:
-		print("fastq-to-fasta-qual.py -i <input fastq> -o <output prefix>")
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt in ("-h", "--help"):
-			print("Split a FASTQ into Seperate FASTA and QUAL files.\nExample usage:\nfastq-to-fasta-qual.py -i <input fastq> -o <output prefix>")
-		elif opt in ("-i", "--input"):
-			fastq = arg
-		elif opt in ("-o", "--outputprefix"):
-			prefix = arg
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--input", type = str, help = "Input FastQ File")
+	parser.add_argument("-o", "--outputprefix", type = str, help = "Prefix to Output Fasta & Qual Files")
+	argsDict = vars(parser.parse_args())
 
-	print(fastq)
+	fastq = argsDict["input"]
+	prefix = argsDict["outputprefix"]
 
 	# Assertions for Required Input
-	assert (len(fastq) != 0), "No FASTQ input provided!"
+	assert (fastq is not None), "No FASTQ input provided!"
 
 	# 
 	# Conversion
 	#
 
 	#  If No Prefix, Use Same as FastQ
-	if len(prefix) < 1: 
-		prefix = fastq.split(".")[0:-1]
+	if prefix is None: 
+		prefix = ".".join(fastq.split(".")[0:-1])
 
 	# Split FastQ into Fasta & Qual
 	SeqIO.convert(fastq, "fastq", prefix + ".fasta", "fasta")
