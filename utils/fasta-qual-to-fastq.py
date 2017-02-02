d81a48a6bd9d1f70414cedb1409ba94630e33985
Greenstick <benacordier@gmail.com>
2017-02-02 11:35:11 -0800

    Version: 0.4.9 - Include FastQ / Fanta Qual converters for Bloocoo

---
+++ utils/fasta-qual-to-fastq.py
@@ -0,0 +1,53 @@
+#! /home/users/cordier/.linuxbrew/bin/python3
+
+if __name__ == "__main__":
+
+
+	# Imports
+	import sys, getopt
+	# Library Import
+	from Bio import SeqIO
+	from Bio.SeqIO.QualityIO import PairedFastaQualIterator
+
+	# Define References
+	fasta = ""
+	qual = ""
+	prefix = ""
+
+	# 
+	# Parse Arguments
+	# 
+
+	try:
+		opts, args = getopt.getopt(argv, "hf:q:o", ["--inputfasta", "--inputqual", "--outputprefix", "--help"])
+	except getopt.GetoptError:
+		print("fasta-qual to fastq.py -f <input fasta> -q <input qual> -o <output prefix>")
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print("Merge Fasta and Qual files into a FastQ file.\nExample usage:\nfasta-qual-to-fastq.py -f <input fasta> -q <input qual> -o <output prefix>")
+		elif opt in ("-f", "--inputfasta"):
+			fasta = arg
+			assert (len(fasta) > 0), "No Fasta input provided!"
+		elif opt in ("-q", "--inputqual"):
+			qual = arg
+			assert (len(qual) > 0), "No Qual input provided!"
+		elif opt in ("-o", "--outputprefix"):
+			prefix = arg
+
+	# 
+	# Conversion
+	#
+
+	#  If No Prefix, Use Same as FASTQ
+	if len(prefix) < 1: 
+		prefix = fasta.split(".")[0:-1]
+
+	# Merge Fasta & Qual into FastQ
+	records = PairedFastaQualIterator(open(fasta), open(qual))
+	SeqIO.write(records, prefix + ".fastq", "fastq")
+
+
+else:
+
+	pass
---
+++ utils/fastq-to-fasta-qual.py
@@ -0,0 +1,76 @@
+#! /home/users/cordier/.linuxbrew/bin/python3
+
+if __name__ == "__main__":
+
+
+	# Imports
+	import sys, getopt
+	# Library Import
+	from Bio import SeqIO
+
+	# Define References
+	fastq = ""
+	prefix = ""
+
+	# 
+	# Parse Arguments
+	# 
+
+	try:
+		opts, args = getopt.getopt(argv, "hi:o", ["--input", "--outputprefix", "--help"])
+	except getopt.GetoptError:
+		print("fastq-to-fasta-qual.py -i <input fastq> -o <output prefix>")
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print("Split a FASTQ into Seperate FASTA and QUAL files.\nExample usage:\nfastq-to-fasta-qual.py -i <input fastq> -o <output prefix>")
+		elif opt in ("-i", "--input"):
+			fastq = arg
+			assert (len(fastq) > 0), "No FASTQ input provided!"
+		elif opt in ("-o", "--outputprefix"):
+			prefix = arg
+
+	# 
+	# Conversion
+	#
+
+	#  If No Prefix, Use Same as FastQ
+	if len(prefix) < 1: 
+		prefix = fastq.split(".")[0:-1]
+
+	# Split FastQ into Fasta & Qual
+	SeqIO.convert(fastq, "fastq", prefix + ".fasta", "fasta")
+	print("Fasta file written to %s.fasta" % prefix)
+	SeqIO.convert(fastq, "fastq", prefix + ".qual", "qual")
+	print("Qual file written to %s.fasta" % prefix)
+	
+
+else:
+
+	pass
+
+
+
+	import sys, getopt
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print 'test.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'test.py -i <inputfile> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print 'Input file is "', inputfile
+   print 'Output file is "', outputfile
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
