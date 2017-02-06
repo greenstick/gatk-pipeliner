b409ef20d5ad535f94ff92abca9c11842eee972e
Greenstick <benacordier@gmail.com>
2017-02-06 12:24:33 -0800

    Version: 0.6.1 - Increase tmp File Handles on samtools collate

--- procedures/bamtofastq.sh
+++ procedures/bamtofastq.sh
@@ -81,7 +81,7 @@ if !(has_state $state); then
 
     format_status "Shuffling & Splitting Merged BAM"
     format_status "Command:\nsamtools collate -uO $dataDir/downloaded/$fileprefix.$subset.$condition.bam $tmp | samtools split -f $dataDir/downloaded/split/$fileprefix.$subset.$condition.%!.%. -"
-    samtools collate -Ou $dataDir/downloaded/$fileprefix.$subset.$condition.bam | samtools split -f $dataDir/downloaded/split/$fileprefix.$subset.$condition.%!.%. -
+    samtools collate -Ou $dataDir/downloaded/$fileprefix.$subset.$condition.bam -n 256 | samtools split -f $dataDir/downloaded/split/$fileprefix.$subset.$condition.%!.%. -
     
     # Update State on Exit
     put_state $? $state
