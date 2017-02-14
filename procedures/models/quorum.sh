7946a2f87d4c02db0c3b03bd5e046ab14fbf80c2
Greenstick <benacordier@gmail.com>
2017-02-14 14:36:29 -0800

    Version: 0.7.2 - Fix...

--- procedures/models/quorum.sh
+++ procedures/models/quorum.sh
@@ -86,7 +86,7 @@ if !(has_state $state); then
 
     # Sort FastQ Inplace
     # Define Command
-    call="fastq-sort −I $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.fastq > $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.sorted.fastq && mv $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.sorted.fastq $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.fastq" 
+    call="fastq-sort −i $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.fastq > $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.sorted.fastq && mv $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.sorted.fastq $dataDir/fastq/split/$fileprefix.$subset.$condition.$readgroup.fastq" 
     # Print & Call
     format_status "Command:\n$call"
     $call
